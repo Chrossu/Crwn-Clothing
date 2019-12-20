06133e04fe00889f3f6557f0abe84140c0def941
Chrossu <Chrossu@users.noreply.github.com>
2019-12-19 23:17:14 -0300

    Implemented react-router-dom into the application

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import store from './redux/store';
@@ -9,8 +10,10 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
-   document.getElementById('root'));
+  document.getElementById('root'));
 
 serviceWorker.unregister();
