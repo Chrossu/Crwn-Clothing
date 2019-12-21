548cba7d9557279d94b61f085b1645329d7b570b
Chrossu <Chrossu@users.noreply.github.com>
2019-12-21 18:23:13 -0300

    Created login form with his corresponding state

--- src/components/login/Login.component.jsx
+++ src/components/login/Login.component.jsx
@@ -2,19 +2,45 @@ import React, { useState } from 'react';
 
 import { StyledLoginContainer, StyledForm, StyledInput, StyledLabel } from './login.style';
 
-const Login = () => (
+const Login = () => {
+  const [state, setState] = useState({
+    email: '',
+    password: ''
+  });
+
+  const { email, password } = state;
+
+  const onChange = e => {
+    const { name, value } = e.target;
+    console.log(state);
+
+    setState({ ...state, [name]: value })
+  }
+
+  const onSubmit = e => {
+    e.preventDefault();
+    setState({
+      email: '',
+      password: ''
+    })
+    console.log('Logineado');
+  }
+
+
+  return (
     <StyledLoginContainer>
-      <StyledForm>
+      <StyledForm onSubmit={onSubmit}>
         {/* Email Input */}
-        <StyledInput name="email" type="email" value={email} required />
-        <StyledLabel></StyledLabel>
+        <StyledInput onChange={onChange} name="email" type="email" value={email} required />
+        <StyledLabel>Email</StyledLabel>
         {/* Password input */}
-        <StyledInput name="password" type="password" value={password} required />
-        <StyledLabel></StyledLabel>
+        <StyledInput onChange={onChange} name="password" type="password" value={password} required />
+        <StyledLabel>Password</StyledLabel>
         {/* SubmitInput */}
         <StyledInput type="submit" value="Submit Form" />
       </StyledForm>
     </StyledLoginContainer>
-)
+  )
+}
 
 export default Login;
--- src/pages/authentication/Authentication.component.jsx
+++ src/pages/authentication/Authentication.component.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import Login from '../../components/login/Login.component';
+
 import { StyledAuthenticationContainer } from './authentication.style';
 
 const AuthenticationPage = () => (
   <StyledAuthenticationContainer>
     Auth page
+    <Login />
   </StyledAuthenticationContainer>
 )
 
