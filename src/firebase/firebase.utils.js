afd1398e559349bc17365963e5ec8a87d64972b5
Chrossu <Chrossu@users.noreply.github.com>
2019-12-21 23:51:55 -0300

    Initialized firebase in the app

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const config = {
+  apiKey: "AIzaSyC9xG-XVX_O5vYBsSZrprzXwLaxNAMvc0U",
+  authDomain: "crown-clothing-7c3fa.firebaseapp.com",
+  databaseURL: "https://crown-clothing-7c3fa.firebaseio.com",
+  projectId: "crown-clothing-7c3fa",
+  storageBucket: "crown-clothing-7c3fa.appspot.com",
+  messagingSenderId: "998938599603",
+  appId: "1:998938599603:web:90f74515a1eef7155150c6",
+  measurementId: "G-HQXHDPETMC"
+};
+
+firebase.initializeApp(config);
