53ee962a3852c23fc17ddd22c8e3216199f8d306
Chrossu <Chrossu@users.noreply.github.com>
2019-12-27 22:49:01 -0300

    Select cartItems and cartItemsCount selectors created

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -4,5 +4,10 @@ const selectCart = state => state.cart;
 
 export const selectCartItems = createSelector(
   [selectCart],
-  
+  cart => cart.cartItems
+)
+
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  cartItems => cartItems.reduce((a, b) => a + b.quantity, 0)
 )
