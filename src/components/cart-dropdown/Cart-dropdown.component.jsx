8c538eed75f84ba78675806aa0a67a3b3599c128
Chrossu <Chrossu@users.noreply.github.com>
2019-12-28 16:53:47 -0300

    Toggle card hidden when go to checkout button is clicked

--- src/components/cart-dropdown/Cart-dropdown.component.jsx
+++ src/components/cart-dropdown/Cart-dropdown.component.jsx
@@ -3,14 +3,14 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { withRouter } from 'react-router-dom';
 
+import { selectCartItems } from '../../redux/cart/cart.selectors';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 import ButtonCustom from '../button-custom/Button-custom.component';
 import CartItem from '../cart-item/Cart-item.component';
 
-import { selectCartItems } from '../../redux/cart/cart.selectors';
-
 import { StyledCartDropdownContainer, StyledCartItems, StyledEmptyMessage } from './cart-dropdown.style'
 
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, toggleCartHidden }) => (
   <StyledCartDropdownContainer>
     <StyledCartItems>
       {
@@ -18,9 +18,15 @@ const CartDropdown = ({ cartItems, history }) => (
           cartItems.map(item => <CartItem id={item.id} item={item} />)
           :
           <StyledEmptyMessage>Your cart is empty</StyledEmptyMessage>
-        }
+      }
     </StyledCartItems>
-    <ButtonCustom onClick={() => history.push("/checkout")}>GO TO CHECKOUT</ButtonCustom>
+    <ButtonCustom
+      onClick={() => {
+        history.push("/checkout");
+        toggleCartHidden();
+        }
+      }
+    >GO TO CHECKOUT</ButtonCustom>
   </StyledCartDropdownContainer>
 )
 
@@ -28,4 +34,4 @@ const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems
 })
 
-export default withRouter(connect(mapStateToProps)(CartDropdown));
+export default withRouter(connect(mapStateToProps, { toggleCartHidden })(CartDropdown));
