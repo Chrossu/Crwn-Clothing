b43b6eef859fc2339a7265a7eec80bd47e543353
Chrossu <Chrossu@users.noreply.github.com>
2019-12-30 23:15:43 -0300

    Persistor integratid into the application

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk'
 import rootReducer from './rootReducer';
 
 const initialState = {};
 
-const store = createStore(rootReducer, initialState, composeWithDevTools(applyMiddleware(thunk)));
+export const store = createStore(rootReducer, initialState, composeWithDevTools(applyMiddleware(thunk)));
 
-export default store;
+export const persistor = persistStore(store);
