79465a7a8d258c1b7d02c37045b30df01248de4a
Chrossu <Chrossu@users.noreply.github.com>
2019-12-31 02:18:20 -0300

    Improved naming of component

---
+++ src/pages/collection/Collection.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import CollectionItem from '../../components/collection-item/Collection-item.component';
+
+import { StyledContainer } from './collection.style'
+
+const CollectionPage = ({ match }) => (
+    <StyledContainer>
+      <h2>Collection Page</h2>
+    </StyledContainer>
+)
+
+export default CollectionPage;
--- src/pages/shop/Shop.component.jsx
+++ src/pages/shop/Shop.component.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 import CollectionsOverview from '../../components/collections-overview/Collections-overview.component';
+import CategoryPage from '../collection/Collection.component'
+
+import { StyledContainer } from './shop.style';
 
 const Shop = ({ match }) => (
-    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <StyledContainer>
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route path={`${match.path}/:collectionId`} component={CategoryPage} />
+    </StyledContainer>
 )
 
 export default Shop;
