a7622af723d1138facb29fac99b248bf22d69e78
Chrossu <Chrossu@users.noreply.github.com>
2019-12-31 15:45:23 -0300

    Created selector for collections items depending on the id of Collection component

--- src/pages/collection/Collection.component.jsx
+++ src/pages/collection/Collection.component.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
+import { selectCollection } from '../../redux/shop/shop.selectors';
 import CollectionItem from '../../components/collection-item/Collection-item.component';
 
 import { StyledContainer } from './collection.style'
 
-const CollectionPage = ({ match }) => (
+const CollectionPage = ({ collections }) => {
+  console.log(collections);
+  return (
     <StyledContainer>
       <h2>Collection Page</h2>
     </StyledContainer>
-)
+)}
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+  collections: selectCollection(ownProps.match.params.collectionId)(state)
+})
+
+export default connect(mapStateToProps)(CollectionPage);
